b23378192fd2c67f02af5906f01c75f71dee7203
prerna bhatra <Prerna-Dev-at-319929695488>
2021-05-22 18:47:48 +0530

    duplocvated in string

---
+++ c++/removecharpresentinsecondstring.cpp
@@ -0,0 +1,5 @@
+Convert first string into a Map. O(N)
+iterate over other string and check if character present in Map from step 1. O(N) + O(1)
+Total time complexity = O(N)
+
+Here you have additional space complexity to store MAP.
